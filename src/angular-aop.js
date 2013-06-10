9ffb5f80573a024e2daa09cc3094d44a8bf5773f
Minko Gechev <mgechev@gmail.com>
2013-06-09 19:25:58 -0700

    afterResolveOf advice added

--- src/angular-aop.js
+++ src/angular-aop.js
@@ -1,102 +1,122 @@
 var AngularAop = angular.module('AngularAOP', []);
 
-/**
- * Service which give access to the pointcuts.
- */
-AngularAop.factory('execute', function Base() {
-    var slice = Array.prototype.slice;
-
+(function (undef) {
     /**
-     * Defines and implements the different advices.
-     *
-     * @constructor
-     * @private
-     * @param {Function} aspect The join point to which the advice should be applied
-     */    
-    function Advices(aspect) {
-        
-        /**
-         * Keeps a reference to the outer, parent context.
-         */        
-        var self = this;
+     * Service which give access to the pointcuts.
+     */
+    AngularAop.factory('execute', ['$q', function Base($q) {
+        var slice = Array.prototype.slice;
 
         /**
-         * The the aspect which will be applied.
+         * Defines and implements the different advices.
+         *
+         * @constructor
+         * @private
+         * @param {Function} aspect The join point to which the advice should be applied
          */
-        this._aspect = aspect;
-        
-        this.before = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments);
-                self._aspect.apply(this, args);
-                return jointPoint.apply(this, args);
+        function Advices(aspect) {
+
+            /**
+             * Keeps a reference to the outer, parent context.
+             */
+            var self = this;
+
+            /**
+             * The the aspect which will be applied.
+             */
+            this._aspect = aspect;
+
+            this.before = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments);
+                    self._aspect.apply(this, args);
+                    return jointPoint.apply(this, args);
+                };
             };
-        };
-        
-        this.after = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments),
-                    result = jointPoint.apply(this, args),
-                    aspectArgs;
-                aspectArgs = [result];
-                self._aspect.apply(this, aspectArgs.concat(args));
-                return result;
+
+            this.after = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments),
+                        result = jointPoint.apply(this, args),
+                        aspectArgs;
+                    aspectArgs = [result];
+                    self._aspect.apply(this, aspectArgs.concat(args));
+                    return result;
+                };
             };
-        };
-        
-        this.around = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments),
-                    aspectArgs,
-                    result;
-                self._aspect.apply(this, aspectArgs);
-                result = jointPoint.apply(this, args);
-                aspectArgs = [result];
-                self._aspect.apply(this, aspectArgs.concat(args));
-                return result;
-            };
-        };
-        
-        this.onThrowOf = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments),
-                    aspectArgs = args;
-                try {
-                    jointPoint.call(this, args);
-                } catch (e) {
-                    aspectArgs.unshift(e);
+
+            this.around = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments),
+                        aspectArgs,
+                        result;
                     self._aspect.apply(this, aspectArgs);
-                }
+                    result = jointPoint.apply(this, args);
+                    aspectArgs = [result];
+                    self._aspect.apply(this, aspectArgs.concat(args));
+                    return result;
+                };
             };
-        };
-        
-        this.onResolveOf = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments),
-                    promise = jointPoint.apply(this, args);
-                promise.then(function () {
-                    self._aspect.apply(this, slice.call(arguments));
-                });
-                return promise;
+
+            this.onThrowOf = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments),
+                        aspectArgs = args;
+                    try {
+                        jointPoint.call(this, args);
+                    } catch (e) {
+                        aspectArgs.unshift(e);
+                        self._aspect.apply(this, aspectArgs);
+                    }
+                };
             };
-        };
-        
-        this.onRejectOf = function (jointPoint) {
-            return function () {
-                var args = slice.call(arguments),
-                    promise = jointPoint.apply(this, args);
-                promise.then(function () {}, function () {
-                    self._aspect.apply(this, slice.call(arguments));
-                });
-                return promise;
+
+            this.onResolveOf = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments),
+                        promise = jointPoint.apply(this, args);
+                    promise.then(function () {
+                        self._aspect.apply(this, slice.call(arguments));
+                    });
+                    return promise;
+                };
             };
+
+            this.afterResolveOf = function (jointPoint) {
+                return function () {
+                    var deferred = $q.defer(),
+                        innerPromise = deferred.promise,
+                        args = slice.call(arguments),
+                        promise = jointPoint.apply(this, args);
+                    promise.then(function () {
+                        var callbackArgs = slice.call(arguments);
+                        innerPromise.then(function () {
+                            self._aspect.apply(this, callbackArgs);
+                        });
+                        deferred.resolve();
+                    });
+                    return innerPromise;
+                };
+            };
+
+            this.onRejectOf = function (jointPoint) {
+                return function () {
+                    var args = slice.call(arguments),
+                        promise = jointPoint.apply(this, args);
+                    promise.then(undefined, function () {
+                        self._aspect.apply(this, slice.call(arguments));
+                    });
+                    return promise;
+                };
+            };
+        }
+
+        return function (aspect) {
+            return new Advices(aspect);
         };
-    }
-    
-    return function (aspect) {
-        return new Advices(aspect);
-    };
-});
+    }]);
+
+}(undefined));
 
 /**
  * Simple aspect which logs all the arguments it recieves
--- src/demo.js
+++ src/demo.js
@@ -3,13 +3,16 @@ TestApp.controller('TestCtrl', function ($scope, TestService) {
     $scope.name = 'foo';
 //    TestService.noSense();
 //    TestService.noSensePromise(42);
+    TestService.noSensePromiseAfter(42).then(function () {
+       console.log('Callback');
+    });
     TestService.noSenseAfter(42);
 });
 
 TestApp.service('TestService', function ($rootScope, $q, execute, Logger) {
     return {
         noSense: execute(Logger).onThrowOf(function () {
-            var nothing = null;
+            var nothing = null  ;
             nothing.call();
         }),
         noSensePromise: execute(Logger).onResolveOf(function (arg) {
@@ -23,7 +26,16 @@ TestApp.service('TestService', function ($rootScope, $q, execute, Logger) {
         }),
         noSenseAfter: execute(Logger).before(execute(Logger).after(function (arg) {                
             return 'Something';
-        }))
+        })),
+        noSensePromiseAfter: execute(Logger).afterResolveOf(function (arg) {
+            var deferred = $q.defer();
+            setTimeout(function () {
+                $rootScope.$apply(function () {
+                    deferred.resolve('Test promise with ' + arg);
+                });
+            }, 1000);
+            return deferred.promise;
+        })
     };
 });
 
