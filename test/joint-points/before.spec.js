84417290712044762f7fbecf27e56664e3aedb47
mgechev <mgechev@gmail.com>
2014-02-28 18:03:08 +0200

    Add test directory

---
+++ test/joint-points/before.spec.js
@@ -0,0 +1,10 @@
+describe('Before joint-point', function () {
+
+  beforeEach(function () {
+    
+  });
+
+  it('should invoke the advice before the wrapper method', function (done) {
+    
+  });
+});
