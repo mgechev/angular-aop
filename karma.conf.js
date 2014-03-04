2e7a00a19ff5afaf877cc4bd85197cdc921e8b1d
mgechev <mgechev@gmail.com>
2014-03-04 13:53:14 +0200

    Add angular mocks to karma

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'src/angular-aop.js',
       'src/aspects/aspect.js',
       'src/**/*.js',
