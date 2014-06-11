c33f845cdb5ea18cdfa9e37d0fa55f6a4385d04d
mgechev <mgechev@gmail.com>
2014-06-11 17:59:13 +0300

    Add tests for before

--- test/joint-points/before.spec.js
+++ test/joint-points/before.spec.js
@@ -15,6 +15,19 @@ describe('Before joint-point', function () {
     expect(before instanceof Aspect).toBeTruthy();
   });
 
-  it('should invoke the advice before the wrapper method', function () {
+  it('should set appropriate value to when', function () {
+    var before = new Aspects[JOINT_POINTS.BEFORE](42);
+    expect(before.when).toBe(JOINT_POINTS.BEFORE);
+  });
+
+  it('should invoke the advice with the appropriate context', function () {
+    var before = new Aspects[JOINT_POINTS.BEFORE](function () {}),
+        params = {
+        method: function () {
+          expect(this).toBe(params.context);
+        },
+        context: {}
+      };
+    expect(before._wrapper(params));
   });
 });
