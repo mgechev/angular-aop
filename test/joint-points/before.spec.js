7ddfd9ce4417eaf9f2b50cd775efe1708d30e695
mgechev <mgechev@gmail.com>
2014-06-11 17:53:22 +0300

    Add tests for the before joint point

--- test/joint-points/before.spec.js
+++ test/joint-points/before.spec.js
@@ -1,10 +1,20 @@
+'use strict';
+
 describe('Before joint-point', function () {
 
   beforeEach(function () {
     
   });
 
-  it('should invoke the advice before the wrapper method', function (done) {
-    
+  it('should be defined', function () {
+    expect(typeof Aspects[JOINT_POINTS.BEFORE]).toBe('function');
+  });
+
+  it('should extend Aspect', function () {
+    var before = new Aspects[JOINT_POINTS.BEFORE](42);
+    expect(before instanceof Aspect).toBeTruthy();
+  });
+
+  it('should invoke the advice before the wrapper method', function () {
   });
 });
