818d42dd364e0d34f7aedee99ff0b0035383ad0e
mgechev <mgechev@gmail.com>
2014-06-18 21:50:01 +0300

    Add tests for around

--- test/joint-points/around.spec.js
+++ test/joint-points/around.spec.js
@@ -6,24 +6,45 @@ describe('Around joint-point', function () {
   });
 
   it('should extend Aspect', function () {
-    var before = new Aspects[JOINT_POINTS.AROUND](42);
-    expect(before instanceof Aspect).toBeTruthy();
+    var around = new Aspects[JOINT_POINTS.AROUND](42);
+    expect(around instanceof Aspect).toBeTruthy();
   });
 
   it('should set appropriate value to when', function () {
-    var before = new Aspects[JOINT_POINTS.AROUND](42);
-    expect(before.when).toBe(JOINT_POINTS.AROUND);
+    var around = new Aspects[JOINT_POINTS.AROUND](42);
+    expect(around.when).toBe(JOINT_POINTS.AROUND);
   });
 
   it('should invoke the advice with the appropriate context', function () {
-    var before = new Aspects[JOINT_POINTS.AROUND](function () {}),
+    var around = new Aspects[JOINT_POINTS.AROUND](function () {}),
         params = {
         method: function () {
           expect(this).toBe(params.context);
         },
         context: {}
       };
-    expect(before._wrapper(params));
+    expect(around._wrapper(params));
+  });
+
+  it('should invoke the advice around the method', function () {
+    var around = new Aspects[JOINT_POINTS.AROUND](function () {
+      adviceCalled += 1;
+      if (adviceCalled === 2) {
+        expect(methodCalled).toBeTruthy();
+      } else {
+        expect(methodCalled).toBeFalsy();
+      }
+    }),
+    params = {
+      method: function () {
+        methodCalled = true;
+        expect(adviceCalled).toBe(1);
+      },
+      context: {}
+    },
+    adviceCalled = 0,
+    methodCalled = false;
+    around._wrapper(params);
   });
 
 });
