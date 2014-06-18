0216f2ef7c20e1d6216537b9ae4ed24afb79bc25
mgechev <mgechev@gmail.com>
2014-06-18 21:48:09 +0300

    Add tests for around

--- test/joint-points/around.spec.js
+++ test/joint-points/around.spec.js
@@ -14,4 +14,16 @@ describe('Around joint-point', function () {
     var before = new Aspects[JOINT_POINTS.AROUND](42);
     expect(before.when).toBe(JOINT_POINTS.AROUND);
   });
+
+  it('should invoke the advice with the appropriate context', function () {
+    var before = new Aspects[JOINT_POINTS.AROUND](function () {}),
+        params = {
+        method: function () {
+          expect(this).toBe(params.context);
+        },
+        context: {}
+      };
+    expect(before._wrapper(params));
+  });
+
 });
