b0ce71069c7900a9c70ef45f0774252f40252809
mgechev <mgechev@gmail.com>
2014-06-18 21:47:14 +0300

    Add around spec

---
+++ test/joint-points/around.spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Around joint-point', function () {
+  it('should be defined', function () {
+    expect(typeof Aspects[JOINT_POINTS.AROUND]).toBe('function');
+  });
+
+});
