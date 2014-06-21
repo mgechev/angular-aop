81259a81ec55ca6b4022c2642f7372fa5439aa9a
mgechev <mgechev@gmail.com>
2014-06-21 13:01:18 +0300

    Add tests for onthrow

--- test/joint-points/onthrow.spec.js
+++ test/joint-points/onthrow.spec.js
@@ -2,4 +2,22 @@
 
 describe('On throw joint-point', function () {
   commonJointpointTests(JOINT_POINTS.ON_THROW);
+
+  it('should invoke the advice after throw of error', function () {
+    var onThrow = new Aspects[JOINT_POINTS.ON_THROW](function () {
+      adviceCalled = true;
+      expect(methodCalled).toBeTruthy();
+    }),
+    params = {
+      method: function () {
+        methodCalled = true;
+        expect(adviceCalled).toBeFalsy();
+        throw 'Error';
+      },
+      context: {}
+    },
+    adviceCalled = false,
+    methodCalled = false;
+    onThrow._wrapper(params);
+  });
 });
