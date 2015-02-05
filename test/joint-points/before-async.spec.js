ba14993ef65794cb737f0084e23f20acfe7363cf
mgechev <mgechev@gmail.com>
2015-02-05 14:32:34 +0200

    Add test for before async

--- test/joint-points/before-async.spec.js
+++ test/joint-points/before-async.spec.js
@@ -3,22 +3,43 @@ describe('After joint-point', function () {
 
   commonJointpointTests(JOINT_POINTS.BEFORE_ASYNC);
 
-  it('should invoke the advice after the method', function () {
-    var resolved = MaybeQ.when(1);
-    var beforeAsync = new Aspects[JOINT_POINTS.BEFORE_ASYNC](function () {
-      adviceCalled = true;
-      expect(methodCalled).toBeFalsy();
-      return resolved;
+  it('should invoke the method after the advice\'s result was resolved',
+    function () {
+      var resolved = MaybeQ.when(1);
+      var beforeAsync = new Aspects[JOINT_POINTS.BEFORE_ASYNC](function () {
+        adviceCalled = true;
+        expect(methodCalled).toBeFalsy();
+        return resolved;
+      });
+      var params = {
+        method: function () {
+          methodCalled = true;
+          expect(adviceCalled).toBeTruthy();
+        },
+        context: {}
+      };
+      var adviceCalled = false;
+      var methodCalled = false;
+      beforeAsync._wrapper(params);
     });
-    var params = {
-      method: function () {
-        methodCalled = true;
-        expect(adviceCalled).toBeTruthy();
-      },
-      context: {}
-    };
-    var adviceCalled = false;
-    var methodCalled = false;
-    beforeAsync._wrapper(params);
-  });
+
+  it('should invoke the method after the advice\'s result was rejected',
+    function () {
+      var rejected = MaybeQ.reject(1);
+      var beforeAsync = new Aspects[JOINT_POINTS.BEFORE_ASYNC](function () {
+        adviceCalled = true;
+        expect(methodCalled).toBeFalsy();
+        return rejected;
+      });
+      var params = {
+        method: function () {
+          methodCalled = true;
+          expect(adviceCalled).toBeTruthy();
+        },
+        context: {}
+      };
+      var adviceCalled = false;
+      var methodCalled = false;
+      beforeAsync._wrapper(params);
+    });
 });
