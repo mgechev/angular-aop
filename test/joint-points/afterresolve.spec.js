d965969b703762864a5d75cd73a1ad10fde36b05
mgechev <mgechev@gmail.com>
2015-02-08 16:54:09 +0200

    Add assert for afterresolve

--- test/joint-points/afterresolve.spec.js
+++ test/joint-points/afterresolve.spec.js
@@ -23,4 +23,34 @@ describe('After resolve joint-point', function () {
       var methodCalled = false;
       onResolve._wrapper(params);
     });
+
+  it('should invoke the advice before the attached to promise' +
+      'method was invoked',
+      function (done) {
+        var onResolve = new Aspects[JOINT_POINTS.AFTER_RESOLVE](function () {
+          adviceCalled = true;
+          expect(methodCalled).toBeTruthy();
+          expect(resolvedPoitcut).toBeTruthy();
+          done();
+        });
+        var params = {
+          method: function () {
+            var res = MaybeQ.when(1);
+            methodCalled = true;
+            expect(adviceCalled).toBeFalsy();
+            expect(resolvedPoitcut).toBeFalsy();
+            return res;
+          },
+          context: {}
+        };
+        var adviceCalled = false;
+        var methodCalled = false;
+        var resolvedPoitcut = false;
+        onResolve._wrapper(params)
+        .then(function () {
+          expect(adviceCalled).toBeFalsy();
+          expect(methodCalled).toBeTruthy();
+          resolvedPoitcut = true;
+        });
+      });
 });
