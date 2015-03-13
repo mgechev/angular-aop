fb44df675f2a6ab768edfd48ad29c1e709b8d6f9
mgechev <mgechev@gmail.com>
2015-03-12 18:05:53 -0700

    Add basic tests for around async

---
+++ test/joint-points/around-async.spec.js
@@ -0,0 +1,29 @@
+describe('Around async joint-point', function () {
+  'use strict';
+
+  commonJointpointTests(JOINT_POINTS.AROUND_ASYNC);
+
+  it('should invoke the advice after the method was resolved',
+    function (done) {
+      var adviceCalled = 0;
+      var onResolve = new Aspects[JOINT_POINTS.AROUND_ASYNC](function () {
+        adviceCalled += 1;
+        if (adviceCalled === 2) {
+          expect(methodCalled).toBeTruthy();
+          done();
+        }
+      });
+      var params = {
+        method: function () {
+          var res = MaybeQ.when(1);
+          methodCalled = true;
+          expect(adviceCalled).toBe(1);
+          return res;
+        },
+        context: {}
+      };
+      var adviceCalled = false;
+      var methodCalled = false;
+      onResolve._wrapper(params);
+    });
+});
